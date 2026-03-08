c2567466e5e1ad3197b2b239aee8a3b69f9c1930
KitCatLoaf <252495354+AnnualMatthewTracker@users.noreply.github.com>
2026-03-08 14:19:53 -0400

    Update addToPathWindows.bat

--- fred/addToPathWindows.bat
+++ fred/addToPathWindows.bat
@@ -1,4 +1,16 @@
 @echo off
-setx PATH "%PATH%;%~dp0"
-echo Fred added to PATH!
+echo Installing Fred...
+
+set "FRED_PATH=%~dp0build"
+
+for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v PATH 2^>nul') do set "CURRENT_PATH=%%B"
+
+if "%CURRENT_PATH%"=="" (
+    reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "%FRED_PATH%" /f
+) else (
+    reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "%CURRENT_PATH%;%FRED_PATH%" /f
+)
+
+echo Done! Please log out and back in, or restart your computer.
+echo Fred will then be available by typing 'fred' anywhere.
 pause
